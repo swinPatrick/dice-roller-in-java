b0700d9e1e0d110efb32dfd41fd1e563247f53b8
Hendrolee <34084278+Hendrolee@users.noreply.github.com>
2020-05-08 14:49:25 +1000

    Add files via upload

--- DiceRollerInJava.java
+++ DiceRollerInJava.java
@@ -30,169 +30,170 @@ public class DiceRollerInJava {
     static final String ANSI_CYAN = "\u001B[36m";
     static final String ANSI_WHITE = "\u001B[37m";
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         DiceRollerInJava dice = new DiceRollerInJava();
         int rollHistory[] = new int[0]; // rollHistory keeps track of ALL rolls the user makes in this session
 
-        // Intro ASCII art
-        System.out.println("///////////////////");
-        System.out.println("/                 /");
-        System.out.println("/   DICE ROLLER   /");
-        System.out.println("/                 /");
-        System.out.println("///////////////////\n");
-        
-        //int userSelection;
         String userInput = "";
-	/*
-        do
+        while(userInput != "exit")
         {
-            // Handles user selection
-            System.out.println("1. Start\n2. How to use\n3. Exit");
-            userSelection = Integer.parseInt(System.console().readLine());
+            // Handle the user input
+            System.out.println("\nWhat would you like to do next? "); // ask user
+            userInput = System.console().readLine(); // read response
+            userInput.toLowerCase(); // change to be readable
 
-            if (userSelection == 1)
+            switch(userInput)
             {
-	*/
-                while(userInput != "exit")
-                {
-                    // Handle the user input
-                    System.out.println("\nWhat would you like to do next? "); // ask user
-                    userInput = System.console().readLine(); // read response
-                    userInput.toLowerCase(); // change to be readable
+                /*
+                    To add new function:
+                    - add keyword and description to 'help' (above 'exit' description)
+                    - add new case for keyword (above 'exit' case)
+                    - comment your function so others know what's what
+                */
+                default:
+                    System.out.println("Option not Recognised... type 'help' to see a list of options.");
+					
+                    break;
 
-                    switch(userInput)
+                // Show the list of options the user can choose from
+                case "help":
+                    System.out.println("Here are your options:");
+                    System.out.println("\troll: roll the dice.");
+					System.out.println("\tcheat: roll the dice to the number you want");
+                    System.out.println("\tcolour: change the colour the terminal prints in.");
+                    System.out.println("\thistory: show the last 10 rolls.");
+                    System.out.println("\texit: leave the program.");
+                    break;
+				
+				
+						
+
+					
+                //Roll the dice normally
+                case "roll": 
+					//Select number of dice to be rolled
+                    System.out.println("How many dice you want to roll: (input number)");
+                    System.out.println("\t1 dice");
+                    System.out.println("\t2 dice");
+                    System.out.println("\t3 dice");
+                    userInput = System.console().readLine();
+                    
+                    int numDice = Integer.parseInt(userInput);
+
+                    if(numDice > 3)
                     {
-                        /*
-                            To add new function:
-                            - add keyword and description to 'help' (above 'exit' description)
-                            - add new case for keyword (above 'exit' case)
-                            - comment your function so others know what's what
-                        */
-                        default:
-                            System.out.println("Option not Recognised... type 'help' to see a list of options.");
-                            break;
-
-                        // Show the list of options the user can choose from
-                        case "help":
-                            System.out.println("Here are your options:");
-                            System.out.println("\troll: roll the dice.");
-                            System.out.println("\tcheat: roll the dice to the number you want");
-                            System.out.println("\tcolour: change the colour the terminal prints in.");
-                            System.out.println("\thistory: show the last 10 rolls.");
-                            System.out.println("\texit: leave the program.");
-                            break;
-
-                        //Roll the dice normally
-                        case "roll": 
-                            // Select the type of dice
-                            String dice6 = "6 Sided dice";
-                            System.setProperty(dice6,"6");
-                            
-                            String dice9 = "9 Sided dice";
-                            System.setProperty(dice9, "9");
-                            
-                            int type;
-                            
-                            System.out.println("Select the type of the dice you want to roll: (input number)");
-                            System.out.println("\t6 sided dice");
-                            System.out.println("\t9 sided dice");
-                            userInput = System.console().readLine(); // read response
-                            
-                            int uInput = Integer.parseInt(userInput);
-                            if (uInput != 6 && uInput != 9)
-                            {
-                                System.out.println("Please select an appropriate dice");
-                                break;
-                            }		
-                            
-                            if (uInput == 6 )
-                            {
-                                type = Integer.getInteger("6 Sided dice");
-                                do
-                                {
-                                    int result = dice.roll(type); // roll the dice and save the result
-                                    dice.draw(result); // show the user what they rolled
-                                    rollHistory = dice.addToHistory(result, rollHistory); // add the roll to history
-                                    System.out.println("Do you want to change the dice? (y/n)");
-                                    userInput = System.console().readLine();
-                                    userInput.toLowerCase(); // change to be readable
-                                }
-                                while (!userInput.equals("y"));
-                            }
-                            
-                            if (uInput == 9 )
-                            {
-                                type = Integer.getInteger("9 Sided dice");
-                                do
-                                {
-                                    int result = dice.roll(type); // roll the dice and save the result
-                                    dice.draw(result); // show the user what they rolled
-                                    rollHistory = dice.addToHistory(result, rollHistory); // add the roll to history
-                                    System.out.println("Do you want to change the dice? (y/n)");
-                                    userInput = System.console().readLine();
-                                    userInput.toLowerCase(); // change to be readable
-                                }
-                                while (!userInput.equals("y"));
-                            }
-                            break;
-                        
-                        // Roll a specific number
-                        case "cheat":
-                            int request = 0;
-                            System.out.println("What number would you like rolled? (1-9): ");
-                            try
-                            {   // try to parse string input into an integer
-                                request = Integer.parseInt(System.console().readLine()); //get user input
-                                if (request >= 1 && request <= 9) //check if valid
-                                {
-                                    dice.draw(request); // show user the dice
-                                    rollHistory = dice.addToHistory(request, rollHistory); // add roll to history
-                                    break;
-                                }
-                            }
-                            catch(Exception e){} // user entered text
-                            // if program reached here user either entered text or a number out of range
-                            System.out.println("Digit not recognised.");
-                            break;
-
-                        // Change the output colour
-                        case "colour":         
-                            dice.SelectColor();
-                            break;
-
-                        // Print the roll history
-                        case "history":
-                            System.out.println("You've rolled " + rollHistory.length + " times!"); // prints roll count
-                            System.out.println("Here al the results of those..."); 
-                            for(int i = 0; i < rollHistory.length; i++) // print each value in rollHistory
-                            {
-                                System.out.println(i+1 + ") " + rollHistory[i]);
-                            }
-                            break;
-
-                        // give the user a nice message before they leave
-                        case "exit":
-                            System.out.println("Going so soon? Bye!");
-                            return;
+                        System.out.println("Maximum of dice allowed is 3!");
+                        break;
                     }
-                }
+                    
+                    // Select the type of dice
+					String dice6 = "6 Sided dice";
+					System.setProperty(dice6,"6");
+					
+					String dice9 = "9 Sided dice";
+					System.setProperty(dice9, "9");
+
+					int type;
+					
+					System.out.println("Select the type of the dice you want to roll: (input number)");
+					System.out.println("\t6 sided dice");
+					System.out.println("\t9 sided dice");
+					userInput = System.console().readLine(); // read response
+
+                    
+					int uInput = Integer.parseInt(userInput);
+                    
+                    if (uInput != 6 && uInput != 9)
+					{
+						System.out.println("Please select an appropriate dice");
+					break;
+					}	
+					
+					if (uInput == 6 )
+					{
+						type = Integer.getInteger("6 Sided dice");
+						do
+						{
+                            for(int i=0; i < numDice; i++)
+                            {
+                            int result = dice.roll(type); // roll the dice and save the result
+							dice.draw(result); // show the user what they rolled
+							rollHistory = dice.addToHistory(result, rollHistory); // add the roll to history
+                            }
+							
+							System.out.println("Do you want to change the dice? (y/n)");
+							userInput = System.console().readLine();
+							userInput.toLowerCase(); // change to be readable
+						}
+						while (!userInput.equals("y"));
+					}
+					
+					
+					if (uInput == 9 )
+					{
+						type = Integer.getInteger("9 Sided dice");
+						do
+						{
+                            for(int i=0; i < numDice; i++)
+                            {
+							    int result = dice.roll(type); // roll the dice and save the result
+							    dice.draw(result); // show the user what they rolled
+                                rollHistory = dice.addToHistory(result, rollHistory); // add the roll to history
+                            }
+							System.out.println("Do you want to change the dice? (y/n)");
+							userInput = System.console().readLine();
+							userInput.toLowerCase(); // change to be readable
+						}
+						while (!userInput.equals("y"));
+					}
+					break;
+				
+
+                // Roll a specific number
+                case "cheat":
+                    int request = 0;
+                    System.out.println("What number would you like rolled? (1-9): ");
+                    try
+                    {   // try to parse string input into an integer
+                        request = Integer.parseInt(System.console().readLine()); //get user input
+                        if(request >= 1 && request <= 9) //check if valid
+                            {
+                                dice.draw(request); // show user the dice
+                                rollHistory = dice.addToHistory(request, rollHistory); // add roll to history
+                                break;
+                            }
+                        
+                    }
+                    catch(Exception e){} // user entered text
+                    // if program reached here user either entered text or a number out of range
+                    System.out.println("Digit not recognised.");
+                    break;
+
+                // Change the output colour
+                case "colour":         
+                    dice.SelectColor();
+                    break;
+
+                // Print the roll history
+                case "history":
+                        System.out.println("You've rolled " + rollHistory.length + " times!"); // prints roll count
+                        System.out.println("Here al the results of those..."); 
+                        for(int i = 0; i < rollHistory.length; i++) // print each value in rollHistory
+                        {
+                            System.out.println(i+1 + ") " + rollHistory[i]);
+                        }
+                        break;
+
+                // give the user a nice message before they leave
+                case "exit":
+                        System.out.println("Going so soon? Bye!");
+                        return;
+
+
             }
-	/*
-            else if (userSelection == 2)
-            {
-                System.out.println("\nType any of the following methods on the terminal when you start");
-                System.out.println("\n 'roll'\n - Roll a 6-faced or 9-faced dice");
-                System.out.println("\n 'cheat'\n - Roll a dice and get the number you specified");
-                System.out.println("\n 'colour'\n - Select a colour you want the dice to be");
-                System.out.println("\n 'history'\n - Shows the result of your last 10 rolls");
-                System.out.println("\n 'help'\n - Lists all functions you can use");
-                System.out.println("\n 'exit'\n - Exits the program\n");
-            }
-        } while (userSelection != 3);
-	*/
+        }
     }
-    
+ 
     // Draw the dice face using ascii characters
     private void draw(int value) {
         int[][] displayVal = faceConfig[value - 1];
@@ -213,11 +214,12 @@ public class DiceRollerInJava {
     }
 
     // Roll the dice in Java
-    private int roll(int type) {
+    private int roll(int type) {        
         Random r = new Random();
         return r.nextInt(type) + 1;
     }
 
+	
     private void SelectColor()
     {
         System.out.println("\n\nSelect a color");
@@ -261,5 +263,5 @@ public class DiceRollerInJava {
             newArray[i] = oldArray[i];
         newArray[oldArray.length] = newValue; // add the new value to the end of the array
         return newArray;
-    }
+    }    
 }
